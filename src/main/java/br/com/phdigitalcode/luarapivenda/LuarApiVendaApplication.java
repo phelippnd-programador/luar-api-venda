6a6b9f540b6b7950ef61fb3e49420c79215e0df5
Phelipp Nascimento <phelipp@DESKTOP-GUIV5F2.localdomain>
2022-06-20 23:02:40 -0300

    inicio projeto

---
+++ src/main/java/br/com/phdigitalcode/luarapivenda/LuarApiVendaApplication.java
@@ -0,0 +1,13 @@
+package br.com.phdigitalcode.luarapivenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuarApiVendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuarApiVendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/phdigitalcode/luarapivenda/LuarApiVendaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.phdigitalcode.luarapivenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuarApiVendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
